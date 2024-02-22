3bebffd824066269344fae61e576a8073f773842
Nursultan Bulekov <bulekov0905@gmail.com>
2024-02-22 18:16:00 +0500

    finish banner slider text

--- frags/banner-slider.php
+++ frags/banner-slider.php
@@ -1,7 +1,22 @@
 <section class="fslider">
-  <div class="slider">
-    <ul class="slides">
-      <li><img src="images/banner1.jpg"></li>
-    </ul>
-  </div>
-</section>
+		<div class="slider">
+			<ul class="slides">
+		    <li>
+		    	<img src="images/banner1.jpg">
+		    	<div class="caption center-align white-text">
+		        <h3 style="font-size: 3rem !important; font-style: bold !important; font-family: 'Bree Serif', serif;">MaxiBurger - Самые вкусные еды!</h3>
+		        <h5 class="light white-text text-lighten-3"><strong>Самые вкусные еды по городу. Мы работаем на качество!</strong></h5>
+		      </div>
+		    </li>
+
+		    <li>
+		    	<img src="images/banner2.jpg">
+		    	<div class="caption center-align white-text">
+		        <h3 style="font-size: 3rem !important; font-style: bold !important; font-family: 'Bree Serif', serif;">Доставка еды!</h3>
+		        <h5 class="light white-text text-lighten-3"><strong>Нам важны время клиентов. Закажи еду с доставкой!</strong></h5>
+		      </div>
+		    </li>
+		    </ul>
+
+	  </div>
+	</section>
