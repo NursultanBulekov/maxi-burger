7dd6126adba04c18645fb1077cf5a6b674db4f27
Nursultan Bulekov <bulekov0905@gmail.com>
2024-02-25 11:14:00 +0500

    connect sections on home page

--- index.php
+++ index.php
@@ -1,8 +1,49 @@
-<?php session_start(); ?>
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
-<html>
-<head><title>Maxi Burger</title></head>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>MAXI BURGER - Fast Food Center</title>
+
+	<!--Linking with fonts and styles  -->
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Yuji+Syuku&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/materialize.min.css">
+	<link rel="stylesheet" href="css/style.css">
+
+
+</head>
 <body>
-  <h1>MAXI BURGER</h1>
+	<!-- Requiring each needed fragments -->
+	<?php require('frags/login-modal.php'); ?>
+	<?php require('frags/register-modal.php'); ?>
+	<?php require('frags/info-modal.php'); ?>
+	<?php require('frags/navbar.php'); ?>
+	<?php require('frags/banner-slider.php'); ?>
+	<?php require('frags/description.php'); ?>
+	<?php require('frags/cards.php'); ?>
+	<?php require('frags/carousel.php'); ?>
+	<?php require('frags/about.php'); ?>
+	<?php require('frags/services.php'); ?>
+	<?php require('frags/reviews.php'); ?>
+	<?php require('frags/footer.php'); ?>
+
+	<script
+	  src="https://code.jquery.com/jquery-3.4.1.min.js"
+	  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+	  crossorigin="anonymous"></script>
+
+    <script src="js/materialize.min.js"></script>
+
+    <script src="js/loaders.js"></script>
+    <script src="js/ajax.js"></script>
 </body>
 </html>
