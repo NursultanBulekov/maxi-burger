007e0bcd2e39ed84d4f02b0b6d5329d368ba9282
Nursultan Bulekov <bulekov0905@gmail.com>
2024-02-27 19:42:00 +0500

    put contacts in footer

--- frags/footer.php
+++ frags/footer.php
@@ -1,3 +1,28 @@
-<footer class="page-footer">
-  <div class="container">MaxiBurger 2021</div>
-</footer>
+<section class="ffooter">
+  <!-- Information about restaurant and links to contact via social networks -->
+		<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="black-text">Свяжитесь с нами</h5>
+                <p class="black-text text-lighten-4">25, Проспект Республики 29, Шымкент</p>
+                <p class="black-text text-lighten-4">Phone : +7 775 445 7123</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="black-text">Социальные сети</h5>
+                <ul>
+                  <li><a class="black-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="black-text text-lighten-3" href="#!">Instagram</a></li>
+                  <li><a class="black-text text-lighten-3" href="#!">Whatsapp</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="black-text container">
+            © 2021 Copyright @ MaxiBurger
+            <a class="black-text text-lighten-4 right" href="#!">Made by Nurs with <span><i class="tiny material-icons">favorite</i></span></a>
+            </div>
+          </div>
+        </footer>
+	</section>
