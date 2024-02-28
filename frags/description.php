3a21583c29f1cd440e253d38a92217018d43d35f
Nursultan Bulekov <bulekov0905@gmail.com>
2024-02-28 19:42:00 +0500

    write restaurant description

---
+++ frags/description.php
@@ -0,0 +1,19 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Фаст фуд центр</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+					<!-- The description of centre -->
+		        	<p>Maxi Burger - фаст фуд центр который предлагает своим клиентам множесвто вариантов еды. 
+						Наш ресторан начал свою работу в 2012 году в городе Шымкент и до сих пор в дружном отношений
+						со всеми своими клиентами. Мы сделали и все еще делаем все возможные удобства для наших клиентов.
+						В сфере обслуживания для клиентов, мы всегда ищем новые возможности и улучшаем наше обслужиание!
+						Спасибо что выбрали нас! С вами команда - MAXI BURGER!
+					</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/415477.png" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
