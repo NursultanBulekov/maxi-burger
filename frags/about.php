1ea4e14b28b7022722d1bf02fa7b31aa7b6771f5
Nursultan Bulekov <bulekov0905@gmail.com>
2024-02-29 13:22:00 +0500

    add text to about section

--- frags/about.php
+++ frags/about.php
@@ -1,5 +1,12 @@
 <section class="fabout">
-  <div class="section white center">
-    <h2>О Нас</h2>
-  </div>
-</section>
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">О Нас</h2>
+		        <p class="grey-text text-darken-3 lighten-3">Ресторан "MaxiBurger" имеет большой опыт  сфере обслуживания и 
+					в целом приготовлений фаст-фудов. Мы  этих сферах около 10 лет и знаем все аспекты. 
+					Главное преимущество нашего ресторана - Уютная атмосфера и вкусные еды.</p>
+				<a href="/maxiburger/about-mxbrgr.php" class="waves-effect waves-light btn" style="background: #ffe001 !important;">
+				Читать больше &raquo;
+			</a>
+		      </div>
+	</section>
