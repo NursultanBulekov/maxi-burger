e45a7e9967e5ae3c8cb29ec7841560cbbcde041e
Nursultan Bulekov <bulekov0905@gmail.com>
2024-02-29 17:46:00 +0500

    create full about page

---
+++ about-mxbrgr.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Maxiburger - About!</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif&display=swap" rel="stylesheet">
+
+
+	<link rel="stylesheet" href="css/materialize.min.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+</head>
+<body>
+
+	<?php require('frags/login-modal.php'); ?>
+
+
+	<?php require('frags/register-modal.php'); ?>
+
+
+	<?php require('frags/info-modal.php'); ?>
+
+
+	<?php require('frags/navbar.php'); ?>
+
+
+	<?php require('frags/carousel.php'); ?>
+
+
+	<?php require('frags/about-us.php'); ?>
+
+
+	<?php require('frags/description.php'); ?>
+
+
+	<?php require('frags/footer.php'); ?>
+
+
+
+
+	<script
+	  src="https://code.jquery.com/jquery-3.4.1.min.js"
+	  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+	  crossorigin="anonymous"></script>
+
+    <script src="js/materialize.min.js"></script>
+
+    <script src="js/loaders.js"></script>
+    <script src="js/ajax.js"></script>
+</body>
+</html>
