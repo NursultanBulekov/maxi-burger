057aee474889e590643eef42f4f407c8f87bbd57
Nursultan Bulekov <bulekov0905@gmail.com>
2024-03-08 18:16:00 +0500

    show service icons

--- frags/services.php
+++ frags/services.php
@@ -1,5 +1,19 @@
 <section class="fservices">
-  <div class="row center">
-    <h5>Наши услуги</h5>
-  </div>
-</section>
+		<div class="row" style="padding-left: 50px; padding-right: 50px;">
+			<div class="col s12 l4 center" style="padding: 50px 50px;" >
+			<img src="images/basket.gif">
+				<!-- <i class="large material-icons" >local_dining</i> -->
+				<h5 class="header" >Разновидность меню</h5>
+			</div>
+			<div class="col s12 l4 center" style="padding: 50px 50px;">
+			<img src="images/truck.gif">
+				<!-- <i class="large material-icons" >local_shipping</i> -->
+				<h5 class="header" >Бесплатная доставка</h5>
+			</div>
+			<div class="col s12 l4 center"  style="padding: 50px 50px;">
+				<img src="images/pizza.gif">
+				<!-- <i class="large material-icons" >mood</i> -->
+				<h5 class="header" >Высокое качество</h5>
+			</div>
+		</div>
+	</section>
