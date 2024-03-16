bdf643238252df37a16fd902824b3bb5da4762dc
Nursultan Bulekov <bulekov0905@gmail.com>
2024-03-16 11:14:00 +0500

    finish food cards

--- frags/cards.php
+++ frags/cards.php
@@ -1,5 +1,67 @@
 <section class="fcards">
-  <div class="row">
-    <div class="card">Бургеры</div>
-  </div>
-</section>
+		<div class="row" style="padding-left: 50px; padding-right: 50px;">
+			<div class="col s12 m12 l4" style="padding: 50px 5px;">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="images/burgers2.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Бургеры<i class="material-icons right">more_vert</i></span>
+				      <div class="card-content">
+			          <p>Узнайте больше о наших бургерах!</p>
+			        </div>
+				    </div>
+				    <div class="card-reveal">
+				      <span class="card-title grey-text text-darken-4">Бургеры<i class="material-icons right">close</i></span>
+				      <p>Наши бургеры считаются одним из лучших в городе. 
+						  Разновидность бургеров впечетляют наших клиентов и они очень вкусные. 
+						  Если не верите, то закажите прямо сейчас и убедитесь в этом сами!</p>
+				    </div>
+				  </div>
+			</div>
+			<div class="col s12 m12 l4" style="padding: 50px 5px;">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="images/burgers8.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Донеры<i class="material-icons right">more_vert</i></span>
+				      <div class="card-content">
+			          <p>Узнайте больше о Донерах</p>
+			        </div>
+				    </div>
+				    <div class="card-reveal">
+				      <span class="card-title grey-text text-darken-4">Донеры<i class="material-icons right">close</i></span>
+				      <p>Донеры - очень популярный вид еды в Турции и во всем мире. 
+						  Сейчас в каждом стране можно найти их но не все вкусны. 
+						  А мы предлагаем нашим клиентам донеров сделанные самими тюрками. 
+						  Вкус восхитительный!</p>
+				    </div>
+				  </div>
+			</div>
+			<div class="col s12 m12 l4" style="padding: 50px 5px;">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="images/burgers7.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Напитки<i class="material-icons right">more_vert</i></span>
+				      <div class="card-content">
+			          <p>Узнайте больше о напитках!</p>
+			        </div>
+				    </div>
+				    <div class="card-reveal">
+				      <span class="card-title grey-text text-darken-4">Напитки<i class="material-icons right">close</i></span>
+				      <p>После каждой еды, человеческому организмому нужна жидкость и 
+						  мы предлагаем множество вариантов напитков. Зайдите в меню напитки и 
+						  закажите их вместе с остальными блюдами.</p>
+				    </div>
+				  </div>
+			</div>
+		</div>
+		<div class="row center" style="margin-bottom: 50px;">
+			<div class="col s12">
+				<a href="food-categories.php" class="waves-effect waves-light btn" style="background: #ffe001 !important;">Больше Еды &raquo;</a>
+			</div>
+		</div>
+	</section>
