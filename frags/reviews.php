8b1027d33f16173780f245c12aa5b3e5adfbe55c
Nursultan Bulekov <bulekov0905@gmail.com>
2024-03-21 19:42:00 +0500

    add customer reviews

--- frags/reviews.php
+++ frags/reviews.php
@@ -1,3 +1,78 @@
 <section class="freviews">
-  <h3 class="center">Отзывы</h3>
-</section>
+
+		<div class="section white center">
+			<h3 class="header">Что говорят о нас наши клиенты?</h3>
+			<div class="carousel myreviews" style="margin-bottom: 35px;">
+			    <a class="carousel-item" href="#one!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ffe001 !important;">
+					        <span class="white-text">"Один из самых вкусных фаст фудов по городу. Каждый раз, когда мне захочется ест что-то вкусное я всегда выбираю здесь."<br>-<br> <strong>Фарида Сарыбаева</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#two!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ffe001 !important;">
+					        <span class="white-text">"Мне тут нравится разновидность меню, что можно выбрать множество еды и атмосфера тоже очень уютная."<br>-<br> <strong>Акбаян Омирзакова</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#three!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ffe001 !important;">
+					        <span class="white-text">"Мы постоянные клиенты этого заведения и любители бургера. Очень круто!"<br>-<br> <strong>Азиза Акбарова</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#four!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ffe001 !important;">
+					        <span class="white-text">"Каждый раз все очень хорошо! Благодарю вас за вашу работу и систему обслуживания! Дети восторге! Все время только сюда ходим."<br>-<br> <strong>Людмила Александрова</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#five!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ffe001 !important;">
+					        <span class="white-text">"Здравствуйте! Нам очень нравится, ходить в ваше заведение со всей семьей! Очень уютно, чисто и конечно же вкусно!"<br>-<br> <strong>Гульнура Курбанова</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#six!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ffe001 !important;">
+					        <span class="white-text">"Администраторы очень вежливые и хорошо работают. Все время в заведениях и помогает своим клиентам. Молодцы! "<br>-<br> <strong>Айгуль Мырзалиевна</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#seven!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ffe001 !important;">
+					        <span class="white-text">"Все на высшем уровне! Спасибо вам за сервис! Вкусные блюда и позитивная атмосфера! Все очень нравится!"<br>-<br> <strong>Меруерт Сагындыкова</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			  </div>
+		</div>
+	</section>
