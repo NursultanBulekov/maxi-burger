24585676c7fcbaad6b8db73355311fb2723ae189
Nursultan Bulekov <bulekov0905@gmail.com>
2024-03-30 11:14:00 +0500

    add small about us fragment

---
+++ frags/about-us.php
@@ -0,0 +1,7 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">О Нас</h2>
+		        <p class="grey-text text-darken-3 lighten-3"></p>
+		      </div>
+	</section>
