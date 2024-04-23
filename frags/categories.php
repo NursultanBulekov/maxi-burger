9c7d82bf3b28536dc1bff81cd69ac1c6b58bc058
Nursultan Bulekov <bulekov0905@gmail.com>
2024-04-23 11:14:00 +0500

    load categories from database

--- frags/categories.php
+++ frags/categories.php
@@ -1,3 +1,57 @@
+<?php
+/* Connecting the categories page with local database*/
+require('backends/connection-pdo.php');
+$sql = 'SELECT * FROM categories';
+$query  = $pdoconn->prepare($sql);
+$query->execute();
+$arr_all = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
 <section class="fcategories">
-  <div class="container"><h3>Меню</h3></div>
+	<div class="container">
+		<div class="section white center">
+			<h3 class="header">Меню</h3>
+		</div>
+<?php if (count($arr_all) == 0) {
+	/* If there are no categories*/
+	echo '<div class="section gray center" style="border: 1px solid black; border-radius: 5px;">
+			<p class="header">Извините. Нет меню для отображения</p>
+		</div>';
+} else {  ?>
+<?php for ($i=1; $i <= count($arr_all); ) { ?>
+		<!-- Displaying the existing categories added by Admin -->
+		<div class="row">
+			<?php for ($j=1; $j <= 10; $j++) { ?>
+				<?php if ($i+$j-2 == count($arr_all)) {
+					break;
+				}  ?>
+			<div class="col s12 m4">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="images/burgers<?php echo $j; ?>.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">
+						  <a class="black-text" href="foods.php?id=<?php echo $arr_all[$i+$j-2]['id']; ?>">
+						  <?php echo $arr_all[$i+$j-2]['name']; ?></a><i class="material-icons right">more_vert</i></span>
+				      <div class="card-content">
+			          <p></p>
+			        </div>
+				    </div>
+				    <div class="card-reveal">
+				      <span class="card-title grey-text text-darken-4"><?php echo $arr_all[$i+$j-2]['name']; ?><i class="material-icons right">
+						  close
+						</i>
+					</span>
+				      <p><?php echo $arr_all[$i+$j-2]['long_desc']; ?></p>
+				    </div>
+				  </div>
+			</div>
+			<?php } ?>
+			<?php $i = $i + 3; ?>
+		</div>
+		<?php 
+				}
+			}
+			 ?>
+	</div>	
 </section>
