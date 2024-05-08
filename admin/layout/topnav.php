73a876c21f6eb7151c4eacf16c6276fc8039f057
Nursultan Bulekov <bulekov0905@gmail.com>
2024-05-08 19:42:00 +0500

    add admin top navigation

---
+++ admin/layout/topnav.php
@@ -0,0 +1,12 @@
+<div class="col s10" id="topnav">
+				<nav>
+				    <div class="nav-wrapper" style="background-color: #668c13">
+				      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+				      <ul class="right hide-on-med-and-down">
+				        <li><a href="dashboard.php">Главное</a></li>
+				        <li><a href="../">Главный сайт</a></li>
+				        <!-- <li data-target="modal1" class="modal-trigger"><a href="#">About</a></li> -->
+				        <li><a href="logout.php">Выйти</a></li>
+				      </ul>
+				    </div>
+				  </nav>
