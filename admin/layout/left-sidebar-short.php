0a4b5702ab8d1f969d0e782a2425ed26fd61e243
Nursultan Bulekov <bulekov0905@gmail.com>
2024-05-11 11:14:00 +0500

    add mobile admin menu

---
+++ admin/layout/left-sidebar-short.php
@@ -0,0 +1,12 @@
+<ul class="sidenav" id="mobile-demo">
+				    <li><a href="food-list.php">Блюда</a></li>
+			        <li><a href="category-list.php">Меню</a></li>
+			        <li><a href="order-list.php">Заказы</a></li>
+			        <!-- <li data-target="modal1" class="modal-trigger"><a href="#">About</a></li> -->
+			        <li><a href="logout.php">Выйти</a></li>
+				  </ul>
+
+
+		<div class="container">
+				<div class="row maincontent">
+					<div class="col s12 center">
