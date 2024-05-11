7ac0d5cbbd90c4c8493317e112dff673db2045e9
Nursultan Bulekov <bulekov0905@gmail.com>
2024-05-11 19:42:00 +0500

    add admin scripts and footer

---
+++ admin/layout/footer.php
@@ -0,0 +1,29 @@
+					</div>
+				</div>
+		</div>
+
+
+
+
+			</div>
+
+
+		</div>
+
+
+
+
+
+
+
+
+	    <script
+		  src="https://code.jquery.com/jquery-3.4.1.min.js"
+		  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+		  crossorigin="anonymous"></script>
+
+	    <script src="../js/materialize.min.js"></script>
+
+	    <script src="../js/loader_admin.js"></script>
+</body>
+</html>
