dd5fb61d278ae7bb18bdee5713fa875a6a52dc79
Nursultan Bulekov <bulekov0905@gmail.com>
2024-05-29 18:16:00 +0500

    add about popup in admin

---
+++ admin/layout/about-modal.php
@@ -0,0 +1,9 @@
+<div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>О нас</h4>
+      <p>-------</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Закрыть</a>
+    </div>
+  </div>
